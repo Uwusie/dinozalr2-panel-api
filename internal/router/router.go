4d44135de3fab6e7f1b62fd8a78e023def8cd030
Kamil Beczek <96703555+KamilBeczek@users.noreply.github.com>
2023-10-24 20:37:02 +0200

    Added DELETE specific wheel
    Created an endpoint and handler function for DELETE specific wheel

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,5 +9,6 @@ import (
 func SetupRoutes(e *echo.Echo) {
 	e.GET("/meow", handlers.Meow)
 	e.GET("/meow/:count", handlers.Meow)
-	e.GET("/wheels/:wheelsId", handlers.Wheels)
+	e.GET("/wheels/:wheelsId", handlers.WheelsGetId)
+	e.DELETE("/wheels/:wheelsId", handlers.WheelsDeleteId)
 }
